7a04bb5aabd4111f62cc358d2fe689abffb6b166
marinakinalone <kinalone.dev@gmail.com>
2021-09-13 19:00:49 +0200

    added linkedin link + fixed link alignment in contact part

--- js/colormode.js
+++ js/colormode.js
@@ -8,15 +8,18 @@ function toggleDarkMode() {
 function iconColorTheme(body) {
    let email = document.getElementById("email_icon")
    let github = document.getElementById("github_icon")
+   let linkedin = document.getElementById("linkedin_icon")
    let instagram = document.getElementById("instagram_icon")
    let heartIcon = document.getElementById("heart")
    email.src = "./img/icon_email_dark.svg"
    github.src = "./img/icon_github_dark.png"
+   linkedin.src = "./img/icon_linkedin_dark.svg"
    instagram.src = "./img/icon_instagram_dark.svg"
    heartIcon.src = "./img/heart_dark.svg"
    if (body.classList == "dark-mode") {
       email.src = "./img/icon_email_light.svg"
       github.src = "./img/icon_github_light.png"
+      linkedin.src = "./img/icon_linkedin_light.svg"
       instagram.src = "./img/icon_instagram_light.svg"
       heartIcon.src = "./img/heart_light.svg"
    }
