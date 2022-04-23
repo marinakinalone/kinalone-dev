a0d301b41cd49bd18c3bdba7841733179b45794e
Marina Kinalone Simonnet <kinalone.dev@gmail.com>
2022-04-23 10:51:36 +0200

    fix styling issues on intro and nav components

--- kinalone-dev/src/components/Navigation.tsx
+++ kinalone-dev/src/components/Navigation.tsx
@@ -1,40 +1,38 @@
 import projectsBtn from '../resources/nav/projects.svg'
 import aboutBtn from '../resources/nav/about.svg'
 import contactBtn from '../resources/nav/contact.svg'
+import moon from '../resources/icons/moon.svg'
+import sun from '../resources/icons/sun.svg'
 
 const Navigation = () => {
   const toggleDarkMode = () => {};
   return (
     <nav className="navigation">
     <ul>
-      <li>
-          <div>
+      <li className="navigation__button">
           <a className="button__frame" href="#projects">
           <img className="button__icon" src={projectsBtn} alt="'projects' section of homepage" />
           projects
           </a>
-          </div>
       </li>
-      <li>
-          <div >
+      <li className="navigation__button">
           <a href="#about" className="button__frame">
           <img className="button__icon" src={aboutBtn} alt="'about' section of homepage" />
           about
             </a>
-          </div>
       </li>
-      <li>
+      <li className="navigation__button">
           <a href="#contact" className="button__frame">
           <img className="button__icon" src={contactBtn} alt="'contact' section of homepage" />
           contact
           </a>
       </li>
-      <li>
+      <li className="navigation__lightswitch">
         <label className="switch" htmlFor="lightswitch">
           <input type="checkbox" id="lightswitch" name="lightswitch" value="dark" onClick={toggleDarkMode} />
           <span className="slider"></span>
-          <img className="toggleicon" id="moon" alt="" src="./img/moon.svg" />
-          <img className="toggleicon" id="sun" alt="" src="./img/sun.svg" />
+          <img className="toggleicon" id="moon" alt="" src={moon} />
+          <img className="toggleicon" id="sun" alt="" src={sun} />
         </label>
       </li>
     </ul>
