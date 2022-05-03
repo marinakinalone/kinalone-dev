0f16f10c88ade701ae4a53d67a989339c8697a18
Marina Kinalone Simonnet <kinalone.dev@gmail.com>
2022-05-03 14:24:05 +0200

    clean codebase

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,10 +1,6 @@
 import portrait from '../resources/marina_portraitparlulo.png'
-import { ReactEventHandler, useState } from 'react';
 
 const Intro = () => {
-
-  const [hover, setHover] = useState(false);
-  const toggleHover = (e:any) => e.target.style.color = "#287773"
   return (
     <section className="intro">
       <img className="intro__portrait" id="marina" src={portrait} alt="drawn portrait of Marina by artist Lu Lo"/>
@@ -12,7 +8,7 @@ const Intro = () => {
         <h1 className="intro__title">Marina Kinalone Simonnet</h1>
         <h2 className="intro__subtitle">full-stack developer</h2>
         <p><span className="bolder">Enthusiastic</span> developer with a <span className="bolder">focus on user experience</span>. Eager learner and researcher, I grow fast in the projects I’m involved with.</p>
-        <p className="intro__copyright">p.s.: drawing is from <a className="link-regular" href="https://www.instagram.com/lulu.xalo/" target="_blank" rel="noopener noreferrer" onMouseOver={toggleHover} >amazing artist Lu Lo</a></p>
+        <p className="intro__copyright">p.s.: drawing is from <a className="link-regular" href="https://www.instagram.com/lulu.xalo/" target="_blank" rel="noopener noreferrer">amazing artist Lu Lo</a></p>
       </div>
   </section>
   )
