266e6a98c7638e665935bfa6327df0afa8b7a595
Marina Kinalone Simonnet <kinalone.dev@gmail.com>
2022-06-03 16:00:22 +0200

    create next app

--- src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-const Header = () => {
-  return (
-    <header className="header">
-      <h1 className="header__title">kinalone.<span className="header__title-last">dev</span></h1>
-    </header>
-  )
-}
-
-export default Header
