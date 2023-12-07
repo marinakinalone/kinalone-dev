8c11ffc8a1920f67fd35a56d5c43859c60cb4c20
Marina Kinalone Simonnet <marina.simonnet@trustly.com>
2023-12-07 16:54:27 +0000

    implement firs half of footer

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -1,7 +1,52 @@
 import React from 'react'
+import styled from 'styled-components'
+import { device } from '../../styles/breakpoints'
+import heartIcon from '../../../public/icons/heart_dark.svg'
+
+const FooterContainter = styled.footer`
+  border-top: ${(props) => props.theme.border.regular};
+  display: flex;
+  justify-content: center;
+  font-family: ${({ theme }) => theme.fontFamily};
+  font-size: ${(props) => props.theme.fontSize.regular.xs};
+  @media ${device.desktopMinWidth} {
+    font-size: ${(props) => props.theme.fontSize.desktop.xs};
+  }
+`
+
+const FooterContent = styled.p``
+
+const FooterIcon = styled.img`
+  max-height: ${(props) => props.theme.fontSize.regular.xs};
+  margin-bottom: -0.1rem;
+  @media ${device.desktopMinWidth} {
+    max-height: ${(props) => props.theme.fontSize.desktop.xs};
+  }
+`
 
 const Footer = () => {
-  return <div>Footer</div>
+  const isDesktop = true // TODO implement isDesktop
+
+  return (
+    <FooterContainter>
+      <FooterContent>
+        {isDesktop && (
+          <>
+            Made with <FooterIcon src={'./icons/heart_dark.svg'} alt="love" /> between Nantes &
+            Sthlm
+          </>
+        )}
+        {/* <FooterText>
+          <FooterIcon src={heartIcon} alt="love" />
+          <FooterTextSpan>between Nantes & Sthlm</FooterTextSpan>
+        </FooterText>
+        <FooterText>
+          <FooterTextSpan>© mks 2022 | </FooterTextSpan>
+          <FooterLink
+            href=" */}
+      </FooterContent>
+    </FooterContainter>
+  )
 }
 
 export default Footer
