330a83add364061e736999ba8826e529fef68510
Marina Kinalone Simonnet <marina.simonnet@trustly.com>
2023-12-07 09:11:24 +0000

    improve app skeleton and add logic for theme toggle

---
+++ app/components/About/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const About = () => {
+  return <div>About t</div>
+}
+
+export default About
---
+++ app/components/Intro/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from '../Hero'
+import Navigation from '../Navigation'
+
+const Intro = () => {
+  return (
+    <>
+      <Hero />
+      <Navigation />
+    </>
+  )
+}
+
+export default Intro
