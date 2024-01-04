16575027c3212c179c62c4e05b85b6b89c5f3ad4
Marina Kinalone Simonnet <marina.simonnet@trustly.com>
2024-01-04 16:24:13 +0000

    add rainbow animation

--- app/components/ui/animations/RainbowText.tsx
+++ app/components/ui/animations/RainbowText.tsx
@@ -1,7 +1,42 @@
 import React from 'react'
+import styled, { keyframes } from 'styled-components'
+
+const changeColor = keyframes`
+  0% {
+    color: #F0D19C;
+  }
+  20% {
+    color: #F0968D;
+  }
+  40% {
+    color: #B968EF;
+  }
+  60% {
+    color: #69ABF0;
+  }
+  60% {
+    color: #78F0AD;
+  }
+  100% {
+    color: #F0D19C;
+  }
+`
+
+const AnimatedLetter = styled.span`
+  display: inline-block;
+  animation: ${changeColor} 1s infinite;
+`
 
 const RainbowText = ({ word }: { word: string }) => {
-  return <span>{word}</span>
+  return (
+    <span>
+      {word.split('').map((letter, index) => (
+        <AnimatedLetter key={index} style={{ animationDelay: `${index * 0.2}s` }}>
+          {letter}
+        </AnimatedLetter>
+      ))}
+    </span>
+  )
 }
 
 export default RainbowText
