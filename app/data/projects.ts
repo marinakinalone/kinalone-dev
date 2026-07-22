d85e51505f1fad106a5e76b298eeccf2f1aaa303
Marina Kinalone Simonnet <kinalone.dev@gmail.com>
2026-07-22 17:41:35 +0100

    add new projects

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -12,44 +12,49 @@ Dimensions: 800x445px
 export const projects = [
   {
     id: 1,
-    title: 'Garden JS',
-    link: 'https://github.com/marinakinalone/garden-js',
-    description:
-      'A cute game to practice JavaScript. Built with React, Redux, SASS Node, Express and MongoDB.',
+    title: 'Portuguese Bits',
+    link: 'https://portuguese-bits.vercel.app/',
+    description: 'A flowery app to practice Portuguese vocabulary.',
   },
   {
     id: 2,
-    title: 'Le Voyage à Stockholm',
-    link: 'https://le-voyage-a-stockholm.onrender.com/',
-    description:
-      'My favorite places in Stockholm, on a map. Built with React, Sass, Node, Express and the Google Maps API.',
+    title: 'Le Journal',
+    link: 'https://le-journal.vercel.app/',
+    description: 'Something between an interactive diary and a video game.',
   },
   {
     id: 3,
-    title: 'La La Dance',
-    link: 'https://la-la-dance.vercel.app/',
-    description:
-      'A personnality test to find the social dance type that suits us best. Built with NextJS, React and React Bootstrap.',
+    title: 'At the Jazz Club',
+    link: 'https://at-the-jazz-club.vercel.app',
+    description: `A jazz-theme game for my dad's birthday.`,
   },
+
+  // TODO replace with Le Voyage à Lisbonne
   {
     id: 4,
-    title: 'Sailor Moon Quote Machine',
-    link: 'https://sailor-moon-quote-machine.vercel.app/',
-    description:
-      'A sailormoon-themed random quote machine. My first React project. Built with React, NextJS and Sass.',
+    title: 'Le Voyage à Stockholm',
+    link: 'https://le-voyage-a-stockholm.onrender.com/',
+    description: 'My favorite places in Stockholm, on a map.',
   },
   // {
   //   id: 5,
-  //   title: 'Project Template',
-  //   link: 'https://github.com/marinakinalone?tab=repositories',
+  //   title: 'La La Dance',
+  //   link: 'https://la-la-dance.vercel.app/',
   //   description:
-  //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget ultricies aliquam, nunc nisl ultricies nunc',
+  //     'A personnality test to find the social dance type that suits us best. Built with NextJS, React and React Bootstrap.',
   // },
   // {
-  //   id: 6,
-  //   title: 'Project Template',
-  //   link: 'https://github.com/marinakinalone?tab=repositories',
+  // id: 6,
+  // title: 'Sailor Moon Quote Machine',
+  //   link: 'https://sailor-moon-quote-machine.vercel.app/',
   //   description:
-  //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget ultricies aliquam, nunc nisl ultricies nunc',
+  //     'A sailormoon-themed random quote machine. My first React project (I am sentimental).',
   // },
+  //  {
+  //  id: 7,
+  //  title: 'Garden JS',
+  //  link: 'https://github.com/marinakinalone/garden-js',
+  //  description: 'A cute game to practice JavaScript.',
+  // },
+  // TODO can add stormi, geeky love letter, and b&b too!
 ]
